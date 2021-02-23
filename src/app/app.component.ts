e0b0804250de26850306adeb68e753506719a20a
PlzCarryMe <riccowiguna1@gmail.com>
2021-02-23 17:52:10 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  a = 0;
+  b = 0;
+
+  item = [
+    { nama: "Jeruk", harga: 15000, satuan: "kg" },
+    { nama: "Apel", harga: 5000, satuan: "kg" }
+  ];
+
+  tampil: boolean = true;
+  tulisanOPEN: String;
+
+  OPEN() {
+    this.tulisanOPEN = "Toko dibuka";
+    alert("Dibuka");
+  }
+
+  values = "";
+  onKey(event: any) {
+    this.values + -event.target.value + '|';
+  }
 }
